f7aac686fa118c80e942eb4fe521ea93ec4046e3
offa <bm-dev@yandex.com>
2017-09-01 16:39:37 +0200

    Installation support added (fixes #11).

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,4 +1,4 @@
 
 install(TARGETS ScopeGuard EXPORT ScopeGuard-config DESTINATION include)
+install(DIRECTORY "${CMAKE_SOURCE_DIR}/include/" DESTINATION include)
 install(EXPORT ScopeGuard-config DESTINATION share/scopeguard/cmake)
-
