b397e24c767f9c055c078c5bd026b73fa586429c
offa <bm-dev@yandex.com>
2017-10-06 14:32:12 +0200

    Create and install a compatible header (withouth extension) to enable drop-in replacement with a future library implementation (#59).

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,4 +1,9 @@
 
 install(TARGETS ScopeGuard EXPORT ScopeGuard-config DESTINATION include)
 install(DIRECTORY "${CMAKE_SOURCE_DIR}/include/" DESTINATION include)
+
+if( ENABLE_COMPAT_HEADER )
+    install(DIRECTORY "${CMAKE_BINARY_DIR}/generated/" DESTINATION include)
+endif()
+
 install(EXPORT ScopeGuard-config DESTINATION share/scopeguard/cmake)
