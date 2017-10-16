89b6a069c8c99ab215fecbbafbea32c5a698e814
offa <bm-dev@yandex.com>
2017-10-16 16:27:39 +0200

    Scope Fail refactored.

--- include/scope_fail.h
+++ include/scope_fail.h
@@ -20,97 +20,41 @@
 
 #pragma once
 
-#include <utility>
-#include <type_traits>
+#include "scope_guard_base.h"
 #include <exception>
 
 namespace sr
 {
+    namespace detail
+    {
+        struct scope_fail_strategy
+        {
+            bool should_execute() const
+            {
+                return uncaught_exceptions() > m_uncaught_on_creation;
+            }
+
+            int uncaught_exceptions() const noexcept
+            {
+                return ( std::uncaught_exception() == true ? 1 : 0 );
+            }
+
+
+            int m_uncaught_on_creation = uncaught_exceptions();
+        };
+    }
+
 
     template<class EF>
-    class scope_fail
+    class scope_fail : public detail::scope_guard_base<EF, detail::scope_fail_strategy>
     {
     public:
 
-        template<class EFP,
-            std::enable_if_t<std::is_constructible<EF, EFP>::value, int> = 0,
-            std::enable_if_t<(!std::is_lvalue_reference<EFP>::value)
-                                && std::is_nothrow_constructible<EF, EFP>::value, int> = 0
-            >
-        explicit scope_fail(EFP&& exitFunction) : m_exitFunction(std::move(exitFunction)),
-                                            m_execute_on_destruction(true),
-                                            m_uncaught_on_creation(uncaught_exceptions())
-        {
-        }
-
-        template<class EFP,
-            std::enable_if_t<std::is_constructible<EF, EFP>::value, int> = 0,
-            std::enable_if_t<std::is_lvalue_reference<EFP>::value, int> = 0
-            >
-        explicit scope_fail(EFP&& exitFunction) try : m_exitFunction(exitFunction),
-                                            m_execute_on_destruction(true),
-                                            m_uncaught_on_creation(uncaught_exceptions())
-        {
-        }
-        catch( ... )
-        {
-            exitFunction();
-            throw;
-        }
-
-        scope_fail(const scope_fail&) = delete;
-
-        template<class T = EF,
-            std::enable_if_t<std::is_nothrow_move_constructible<T>::value, int> = 0
-            >
-        scope_fail(scope_fail&& other) noexcept(std::is_nothrow_move_constructible<T>::value || std::is_nothrow_copy_constructible<T>::value)
-                                        : m_exitFunction(std::move(other.m_exitFunction)),
-                                        m_execute_on_destruction(other.m_execute_on_destruction),
-                                        m_uncaught_on_creation(uncaught_exceptions())
-        {
-            other.release();
-        }
-
-        template<class T = EF,
-            std::enable_if_t<!std::is_nothrow_move_constructible<T>::value, int> = 0
-            >
-        scope_fail(scope_fail&& other) noexcept(std::is_nothrow_move_constructible<T>::value || std::is_nothrow_copy_constructible<T>::value)
-                                        : m_exitFunction(other.m_exitFunction),
-                                        m_execute_on_destruction(other.m_execute_on_destruction),
-                                        m_uncaught_on_creation(other.m_uncaught_on_creation)
-        {
-            other.release();
-        }
-
-        ~scope_fail() noexcept(true)
-        {
-            if( ( m_execute_on_destruction == true ) && ( uncaught_exceptions() > m_uncaught_on_creation ) )
-            {
-                m_exitFunction();
-            }
-        }
-
-
-        void release() noexcept
-        {
-            m_execute_on_destruction = false;
-        }
-
-
-        scope_fail& operator=(const scope_fail&) = delete;
-        scope_fail& operator=(scope_fail&&) = delete;
+        using detail::scope_guard_base<EF, detail::scope_fail_strategy>::scope_guard_base;
 
 
     private:
 
-        int uncaught_exceptions() const noexcept
-        {
-            return ( std::uncaught_exception() == true ? 1 : 0 );
-        }
-
-        EF m_exitFunction;
-        bool m_execute_on_destruction;
-        int m_uncaught_on_creation;
     };
 
 
