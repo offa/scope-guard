ddabea3b88e5b4e1a2fd2076ae4f3d1f8c65bc8c
offa <bm-dev@yandex.com>
2017-10-18 16:10:47 +0200

    Using new trait helper variables.

--- include/unique_resource.h
+++ include/unique_resource.h
@@ -30,8 +30,8 @@ namespace sr
     namespace detail
     {
         template<class T, class TT>
-        using is_ntmocp_constructible = std::conditional_t<std::is_reference<TT>::value
-                                                            || !std::is_nothrow_move_constructible<TT>::value,
+        using is_ntmocp_constructible = std::conditional_t<std::is_reference_v<TT>
+                                                            || !std::is_nothrow_move_constructible_v<TT>,
                                                         typename std::is_constructible<T, const TT&>::type,
                                                         typename std::is_constructible<T, TT>::type>;
 
