00660c5d5f5d61bcd9fbd77d4513221b360d77a6
offa <bm-dev@yandex.com>
2017-10-19 16:26:42 +0200

    Cleanup.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -12,8 +12,6 @@ CMAKE_INSTALLER=install-cmake.sh
 
 if [[ ! -f ${CMAKE_INSTALLER} ]]
 then
-    echo "CMake installer not available, downloading ..."
-
     curl -sSL https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh -o ${CMAKE_INSTALLER}
     chmod +x ${CMAKE_INSTALLER}
 fi
