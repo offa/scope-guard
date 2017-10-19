6f178e68d20243dac56d7ae8a44bb31c5a6f6da0
offa <bm-dev@yandex.com>
2017-10-19 13:55:25 +0200

    Installation of CMake on Travis moved to script.

---
+++ script/ci_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BUILD_DIR=${TRAVIS_BUILD_DIR}
+
+## Install CMake
+mkdir -p "${DEPENDENCY_DIR}" && cd "${DEPENDENCY_DIR}"
+
+if [[ ! -d "$(ls -A ${DEPENDENCY_DIR}/cmake)" ]]; then
+    CMAKE_URL="https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz"
+    mkdir -p cmake
+    wget --no-check-certificate --quiet -O - "${CMAKE_URL}" | tar --strip-components=1 -xz -C cmake
+fi
+
+export PATH="${DEPENDENCY_DIR}/cmake/bin:${PATH}"
+cmake --version
+
+cd ${BUILD_DIR}
+
