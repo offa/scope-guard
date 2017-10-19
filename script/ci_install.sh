65b60a942dee215677f4d4412a4cd9462e3180a9
offa <bm-dev@yandex.com>
2017-10-19 14:57:32 +0200

    Creation of directories is not related to cmake installation.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -2,9 +2,10 @@
 
 BUILD_DIR=${TRAVIS_BUILD_DIR}
 
-## Install CMake
 mkdir -p "${DEPENDENCY_DIR}" && cd "${DEPENDENCY_DIR}"
 
+
+## Install CMake
 if [[ ! -d "$(ls -A ${DEPENDENCY_DIR}/cmake)" ]]; then
     CMAKE_URL="https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz"
     mkdir -p cmake
