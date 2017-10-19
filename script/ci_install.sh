ca90f58f331430433e2b15b8344a7a6ff9df47be
offa <bm-dev@yandex.com>
2017-10-19 15:12:39 +0200

    Formatting.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -17,7 +17,6 @@ then
     curl -sSL https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh -o ${CMAKE_INSTALLER}
     chmod +x ${CMAKE_INSTALLER}
     sudo ./install-cmake.sh --prefix=/usr/local --skip-license
-
 else
     echo "CMake installer available"
 fi
