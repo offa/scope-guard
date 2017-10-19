7e60ff3d60389b4dab9568ed3757706e56706e13
offa <bm-dev@yandex.com>
2017-10-19 15:44:28 +0200

    Debug code removed.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -16,8 +16,6 @@ then
 
     curl -sSL https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh -o ${CMAKE_INSTALLER}
     chmod +x ${CMAKE_INSTALLER}
-else
-    echo "CMake installer available"
 fi
 
 sudo ./${CMAKE_INSTALLER} --prefix=/usr/local --skip-license
