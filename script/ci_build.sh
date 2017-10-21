300faf40e837b9a6723ec84292ddb34364472dcb
offa <bm-dev@yandex.com>
2017-10-21 20:24:57 +0200

    Clang Linkerflags.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -3,12 +3,13 @@
 set -ex
 
 if [[ "${CXX}" == clang* ]]; then
-    export CXXFLAGS="-stdlib=libc++ -lc++abi"
+    export CXXFLAGS="-stdlib=libc++"
+    EXTRA="-DCMAKE_EXE_LINKER_FLAGS=-lc++abi -lc++"
 fi
 
 mkdir build && cd build
 
-cmake ..
+cmake ${EXTRA} ..
 make
 make unittest
 
