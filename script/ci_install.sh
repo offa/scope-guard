a31dcbac2092afed571d231ae8c41c4eb068b92b
offa <bm-dev@yandex.com>
2017-10-21 20:32:42 +0200

    Build parameters.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -33,12 +33,20 @@ then
         git clone --depth=1 -b ${LLVM_RELEASE} https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
         git clone --depth=1 -b ${LLVM_RELEASE} https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
 
+        if [[ -z "${BUILD_32_BITS}" ]]
+        then
+            export BUILD_32_BITS=OFF
+            echo disabling 32 bit build
+        fi
+
         mkdir -p build && cd build
 
         cmake -DCMAKE_C_COMPILER=${CC} \
-                -DCMAKE_CXX_COMPILER=${CXX} \
                 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
                 -DCMAKE_INSTALL_PREFIX=/usr \
+                -DLIBCXX_ABI_UNSTABLE=ON \
+                -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
+                -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
                 ../llvm-source
         make cxx -j4
     else
