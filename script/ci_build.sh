2028f649d770d95e2a9cd3e9253f5f05b3446e48
offa <bm-dev@yandex.com>
2019-05-29 20:41:46 +0200

    Workaround for #156.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -6,6 +6,10 @@ set -ex
 apt-get install -y python3-pip
 pip3 install conan
 
+# #156 Workaround for conan#4938
+mkdir -p ~/.conan
+cp script/settings.yml ~/.conan/
+
 
 if [[ "${CXX}" == clang* ]]
 then
