8610b6ad52e9f4a2dad66d8be8d82d7431b2f801
offa <bm-dev@yandex.com>
2019-05-29 19:44:25 +0200

    Install python, pip and conan for CI (#156).

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,12 +2,18 @@
 
 set -ex
 
+# Conan
+apt-get install -y python3-pip
+pip3 install conan
+
+
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
 fi
 
 mkdir build && cd build
+conan install --build missing ..
 
 cmake ..
 make
