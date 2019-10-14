b3e6a9cb993bb178ec9cbd67d42c785eca5e278e
offa <bm-dev@yandex.com>
2019-10-14 17:43:32 +0200

    Fix old GCC ABI used by Conan (#162).

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -5,20 +5,18 @@ set -ex
 # Conan
 apt-get install -y python3-pip
 pip3 install conan
-
+conan profile new default --detect
 
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
-    CONAN_STDLIB=libc++
 else
-    CONAN_STDLIB=libstdc++11
+    conan profile update settings.compiler.libcxx=libstdc++11 default
 fi
 
 mkdir build && cd build
 
 conan install \
-    -s compiler.libcxx=${CONAN_STDLIB} \
     -g cmake_find_package \
     -g cmake_paths \
     ..
