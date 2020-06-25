151535e269d8f3385fad92e0c712cd1b855d2ca4
offa <bm-dev@yandex.com>
2020-06-25 14:22:53 +0200

    Limit conan ci build to x86_64.

--- script/conan_build.sh
+++ script/conan_build.sh
@@ -3,6 +3,8 @@
 set -ex
 
 export CONAN_DOCKER_IMAGE=conanio/gcc${CONAN_GCC_VERSIONS}
+export CONAN_ARCHS="x86_64"
+export CONAN_BUILD_TYPES=Release
 
 pip install -U conan conan_package_tools
 python build.py
