243ebbc963f56abc704552bf2ebb5013683204ae
offa <bm-dev@yandex.com>
2020-12-16 17:12:01 +0100

    Handwritten test main replaced.

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -22,32 +22,3 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
-#include <trompeloeil.hpp>
-
-
-namespace trompeloeil
-{
-    template <>
-    void reporter<specialized>::send(severity s, const char* file, unsigned long line, const char* msg)
-    {
-        std::ostringstream os;
-
-        if( line != 0 )
-        {
-            os << file << ':' << line << '\n';
-        }
-
-        os << msg;
-        const  auto failure = os.str();
-
-        if( s == severity::fatal )
-        {
-            FAIL(failure);
-        }
-        else
-        {
-            CAPTURE(failure);
-            CHECK(failure.empty());
-        }
-    }
-}
