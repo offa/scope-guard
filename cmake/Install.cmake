b51cd3c2d65c21a0540d9e656842b4d8fe24cc8e
offa <bm-dev@yandex.com>
2021-05-27 21:37:02 +0200

    Use relative CMake paths (#182).

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,9 +1,9 @@
 
 install(TARGETS ScopeGuard EXPORT ScopeGuard-config DESTINATION include)
-install(DIRECTORY "${CMAKE_SOURCE_DIR}/include/" DESTINATION include)
+install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/" DESTINATION include)
 
 if( ENABLE_COMPAT_HEADER )
-    install(DIRECTORY "${CMAKE_BINARY_DIR}/generated/" DESTINATION include)
+    install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/generated/" DESTINATION include)
 endif()
 
 install(EXPORT ScopeGuard-config DESTINATION share/scopeguard/cmake)
