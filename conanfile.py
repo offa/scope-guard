fe934dd98f981ac0947429cf0903d32e5b7571ee
offa <bm-dev@yandex.com>
2022-07-19 19:01:09 +0200

    Update Catch v3.1.0

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class ScopeguardConan(ConanFile):
         "enable_compat_header=OFF"
     )
     __requirements = [
-        "catch2/3.0.1",
+        "catch2/3.1.0",
         "trompeloeil/42"
     ]
 
