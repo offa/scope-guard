f48e47464961fb13c935194e47e3c26ecfe5a589
Francois Carouge <francois.carouge@gmail.com>
2022-12-18 19:43:31 -0800

    [cmake] fix find package name and path mixed typography

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -1,9 +1,9 @@
 
-install(TARGETS ScopeGuard EXPORT ScopeGuard-config DESTINATION include)
+install(TARGETS ScopeGuard EXPORT ScopeGuardConfig DESTINATION include)
 install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/" DESTINATION include)
 
 if( ENABLE_COMPAT_HEADER )
     install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/generated/" DESTINATION include)
 endif()
 
-install(EXPORT ScopeGuard-config DESTINATION share/scopeguard/cmake)
+install(EXPORT ScopeGuardConfig DESTINATION share/ScopeGuard/cmake)
