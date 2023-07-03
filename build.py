248ac70728e8ec6a61b5e8d31d9b63f9889f6c9c
offa <bm-dev@yandex.com>
2023-07-03 18:04:59 +0200

    Remove CPT leftover (#195)

--- build.py
+++
@@ -1,9 +0,0 @@
-from cpt.packager import ConanMultiPackager
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds()
-    builder.remove_build_if(
-        lambda build: build.settings["compiler"] == "gcc" and build.settings[
-            "compiler.libcxx"] == "libstdc++")
-    builder.run()
