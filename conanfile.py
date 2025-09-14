c3158023280809dbc9acf6757d5422bc72c1405d
offa <8887756+offa@users.noreply.github.com>
2025-09-14 11:56:33 +0000

    Format Conanfile with ruff (#245)

--- conanfile.py
+++ conanfile.py
@@ -11,38 +11,46 @@ class ScopeguardConan(ConanFile):
     license = "MIT"
     author = "offa <offa@github>"
     url = "https://github.com/offa/scope-guard"
-    description = "Implementation of Scoped Guards and Unique Resource as proposed in P0052."
+    description = (
+        "Implementation of Scoped Guards and Unique Resource as proposed in P0052."
+    )
     homepage = "https://github.com/offa/scope-guard"
-    topics = ("cpp", "cpp17", "p0052", "scope-guard",
-              "scope-exit", "scope-fail", "scope-success", "unique-resource", "cmake")
+    topics = (
+        "cpp",
+        "cpp17",
+        "p0052",
+        "scope-guard",
+        "scope-exit",
+        "scope-fail",
+        "scope-success",
+        "unique-resource",
+        "cmake",
+    )
     settings = "os", "arch", "compiler", "build_type"
     exports = ["LICENSE"]
     exports_sources = ("CMakeLists.txt", "include/*", "test/*", "cmake/*")
     package_type = "header-library"
-    options = {
-        "unittest": [True, False],
-        "enable_compat_header": [True, False]
-    }
-    default_options = {
-        "unittest": False,
-        "enable_compat_header": False
-    }
-    __requirements = [
-        "catch2/3.10.0",
-        "trompeloeil/49"
-    ]
+    options = {"unittest": [True, False], "enable_compat_header": [True, False]}
+    default_options = {"unittest": False, "enable_compat_header": False}
+    __requirements = ["catch2/3.10.0", "trompeloeil/49"]
 
     def set_version(self):
-        cmake_lists_content = load(self, os.path.join(self.recipe_folder, "CMakeLists.txt"))
-        project_match = re.search(r'project\s*\((.+?)\)', cmake_lists_content, re.DOTALL)
+        cmake_lists_content = load(
+            self, os.path.join(self.recipe_folder, "CMakeLists.txt")
+        )
+        project_match = re.search(
+            r"project\s*\((.+?)\)", cmake_lists_content, re.DOTALL
+        )
 
         if not project_match:
-            raise ConanInvalidConfiguration("No valid project() statement found in CMakeLists.txt")
+            raise ConanInvalidConfiguration(
+                "No valid project() statement found in CMakeLists.txt"
+            )
 
         project_params = project_match.group(1).split()
         version_string = project_params[project_params.index("VERSION") + 1]
 
-        if not re.search(r'\d+\.\d+\.\d+(?:\.\d)?', version_string):
+        if not re.search(r"\d+\.\d+\.\d+(?:\.\d)?", version_string):
             raise ConanInvalidConfiguration("No valid version found in CMakeLists.txt")
 
         self.version = version_string
@@ -56,7 +64,9 @@ class ScopeguardConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["SCOPEGUARD_UNITTEST"] = self.options.unittest
-        tc.variables["SCOPEGUARD_ENABLE_COMPAT_HEADER"] = self.options.enable_compat_header
+        tc.variables["SCOPEGUARD_ENABLE_COMPAT_HEADER"] = (
+            self.options.enable_compat_header
+        )
         tc.generate()
 
         deps = CMakeDeps(self)
@@ -67,7 +77,12 @@ class ScopeguardConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, pattern="LICENSE", dst=os.path.join(self.package_folder, "licenses"), src=self.source_folder)
+        copy(
+            self,
+            pattern="LICENSE",
+            dst=os.path.join(self.package_folder, "licenses"),
+            src=self.source_folder,
+        )
         cmake = self._configure_cmake()
         cmake.install()
 
