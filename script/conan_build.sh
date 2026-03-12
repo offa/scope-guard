c06a89456297187dfc002631299130d9636586d8
offa <8887756+offa@users.noreply.github.com>
2026-03-12 14:11:39 +0000

    Migrate to Conan Action (#262)

--- script/conan_build.sh
+++ script/conan_build.sh
@@ -2,10 +2,4 @@
 
 set -ex
 
-export PATH=$HOME/.local/bin:$PATH
-apt-get update
-apt-get install -y pipx
-pipx install conan
-
-conan profile detect
 conan create -o unittest=True --build=missing .
