df3dcbc55df41680e87c540e1954bfe4c7dc35f7
offa <8887756+offa@users.noreply.github.com>
2026-06-16 20:18:24 +0000

    Update Catch 3.15.1 (#270)

--- conanfile.py
+++ conanfile.py
@@ -56,7 +56,7 @@ class ScopeguardConan(ConanFile):
 
     def requirements(self):
         if self.options.unittest:
-            self.requires("catch2/3.15.0")
+            self.requires("catch2/3.15.1")
             self.requires("trompeloeil/49")
 
     def generate(self):
